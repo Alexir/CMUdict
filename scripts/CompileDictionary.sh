d7268b8e8aeed9da936e347b9468cf374738fdaf
Alex Rudnicky <air@cs.cmu.edu>
2014-12-25 19:18:27 -0500

    upadtes

--- scripts/CompileDictionary.sh
+++ scripts/CompileDictionary.sh
@@ -1,21 +1,21 @@
-#!sh
+#!/usr/bin/bash
 # [20080422] (air) Compile cmudict into SPHINX_40 form
-# [20100118] (air) 
+# [20100118] (air)
+# [20141225] (air) some house keeping
+
 
 DIR=sphinxdict
 DICT_BASE=cmudict
-DICTIONARY=${DICT_BASE}.0.7a
-
-
-
-echo "Compiling $DICTIONARY..."
+DICTIONARY=${DICT_BASE}-0.7b  # note that this hard-coded; update as versions creep up.
+echo "Compiling $DICTIONARY"; echo ""
 
 # make_baseforms.pl removes stress marks and eliminates resulting duplicates
+echo "Collapse stress information..."
 perl ./scripts/make_baseform.pl $DICTIONARY $DIR/$$_SPHINX_40
+echo "" 
 
 
-echo ""
-echo "Testing sphinx cmudict... "
+echo "Do final cmudict_SPHINX check... "
 if ./scripts/test_dict.pl -p $DIR/SphinxPhones_40 $DIR/$$_SPHINX_40
 then
     cp -p $DIR/$$_SPHINX_40 $DIR/${DICT_BASE}_SPHINX_40
@@ -26,11 +26,12 @@ else
     if [ -e $DIR/${DICTIONARY}_SPHINX_40 ] ; then  rm $DIR/${DICTIONARY}_SPHINX_40 ; fi
     echo ""
     echo "$0 encountered errors"
-    echo "dictionary compilation not completed"
+    echo "DICTIONARY COMPILATION NOT COMPLETED"
+    exit
 fi
-exit
-rm $DIR/$$_SPHINX_40
+echo ""
 
-echo "Done"
+rm $DIR/$$_SPHINX_40
+echo "Done."
 
 #
